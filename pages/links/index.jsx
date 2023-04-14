190dcb96f2a436f65f4ffd87c9cc2d7dbac9ae0d
Eduardo Zaqueu <zaq1@MacBook-Air-de-Eduardo.local>
2023-04-14 13:57:25 -0300

    update

--- pages/links/index.jsx
+++ pages/links/index.jsx
@@ -1,40 +1,49 @@
 import React from 'react'
-import Link from 'next/Link'
 import Image from 'next/image'
 
 function Links() {
   return (
     <div className='linkspage-container'>
-      <div className="bg" />
+      <div className="bg animated" />
 
-      <Link href='/'>
-        <div className="link-card card1">
-          <span className='link-card-title'>Acesse nosso</span>
-          <span className='link-card-sub'>SITE OFICIAL</span>
+      <div className="logo-wrapper">
+        <div className="internal-wrapper">
+          <Image className='logo linkslogo' src='/../public/images/logo.png' width='450' height='450' alt='image'></Image>
         </div>
-      </Link>
+      </div>
 
-      <Link href='/'>
-        <div className="link-card card2">
-          <span className='link-card-title'>Conheça nossos</span>
-          <span className='link-card-sub'>SERVIÇOS</span>
-        </div>
-      </Link>
+      <div className="linkcards-wrapper">
+        <a href='https://smcreativemarketing.com.br/inicio' target='_blank'>
+          <div className="link-card card1">
+            <span className='link-card-title'>Acesse nosso</span>
+            <span className='link-card-sub'>SITE OFICIAL</span>
+          </div>
+        </a>
 
-      <a href='www.tiktok.com/@smcreativemkt' target='_blank'>
-        <div className="link-card card3">
-          <span className='link-card-title'>Siga-nos no</span>
-          <span className='link-card-sub'>TIKTOK</span>
-        </div>
-      </a>
+        <a href='https://smcreativemarketing.com.br/sm' target='_blank'>
+          <div className="link-card card2">
+            <span className='link-card-title'>Conheça nossos</span>
+            <span className='link-card-sub'>SERVIÇOS</span>
+          </div>
+        </a>
 
-      <a href=' https://api.whatsapp.com/send?phone=5524998568180' target='_blank'>
-        <div className="link-card-small card-contact">
-          <span className='link-card-contact'>FALE CONOSCO!</span>
-        </div>
-      </a>
+        <a href='www.tiktok.com/@smcreativemkt' target='_blank'>
+          <div className="link-card card3">
+            <span className='link-card-title'>Siga-nos no</span>
+            <span className='link-card-sub'>TIKTOK</span>
+          </div>
+        </a>
 
+        <a href='https://api.whatsapp.com/send?phone=5524998568180' target='_blank'>
+          <div className="link-card-small card-contact">
+            <span className='link-card-contact'>FALE CONOSCO</span>
+          </div>
+        </a>
+      </div>
 
+      <div className="footer-links">
+        <h2>SM Creative Marketing - Todos os direitos reservados</h2>
+      </div>
     </div>
   )
 }
