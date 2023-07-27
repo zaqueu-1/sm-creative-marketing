a0f956751d285acc28d24169e805ede310b7e632
Gabriel Dias <g.dias1@hotmail.com>
2023-07-27 09:33:50 -0300

    Manutenção layout

--- pages/Hero.jsx
+++ pages/Hero.jsx
@@ -15,9 +15,9 @@ function Hero() {
         <h2 data-aos={"fade-right"} data-aos-duration={"1200"} className='home-text'>BOAS IDEIAS</h2>
         <h2 data-aos={"fade-right"} data-aos-duration={"1500"} className='home-text'>DEVEM SEMPRE</h2>
         <h2 data-aos={"fade-left"} data-aos-duration={"1800"} className='home-bigtext'>BRILHAR</h2>
-        <div data-aos={"fade-up"} data-aos-duration={"1500"} className="controls">
+        {/* <div data-aos={"fade-up"} data-aos-duration={"1500"} className="controls"> */}
           <button onClick={goTo('https://wa.me/5524998568180')} className="talk-btn">FALE CONOSCO</button>
-          <div className="social-media">
+          {/* <div className="social-media"> */}
             <a target='_blank' href='https://www.facebook.com/sabrynasm06?mibextid=LQQJ4d'>
               <CiFacebook className='icon' />
             </a>
@@ -27,13 +27,13 @@ function Hero() {
             <a target='_blank' href='https://www.tiktok.com/@smcreativemkt'>
               <TbBrandTiktok className='icon' />
             </a>
-          </div>
-        </div>
+          {/* </div> */}
+        {/* </div> */}
       </div>
 
-      <div data-aos={"fade-up"} data-aos-duration={"1500"} className="image-wrapper">
+      {/* <div data-aos={"fade-up"} data-aos-duration={"1500"} className="image-wrapper">
         <Image width='600' height='600' className='logo' src={require('../public/images/logo.png')} alt='logo' />
-      </div>
+      </div> */}
 
     </div>
   )
